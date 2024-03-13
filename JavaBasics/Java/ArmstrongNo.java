070038806dfe861065d089bc47da9b793555c9bc
anushkaagarwal15 <raanushka15@gmail.com>
2024-03-13 16:20:43 +0530

    lengthof string

---
+++ JavaBasics/Java/ArmstrongNo.java
@@ -0,0 +1,10 @@
+package JavaBasics.Java;
+
+
+public class ArmstrongNo{
+    public static void main(String args[]){
+
+        System.out.println("hey");
+
+    }
+}
---
+++ JavaBasics/Strings/LengthOfString.java
@@ -0,0 +1,19 @@
+package Strings;
+
+/**
+ * LengthOfString
+ */
+public class LengthOfString {
+
+    public static int lengthString(String str){
+ return str.length();
+
+    }
+
+    public static void main(String args[]){
+
+        String str = "Geeks";
+        lengthString(str);
+    }
+    
+}
